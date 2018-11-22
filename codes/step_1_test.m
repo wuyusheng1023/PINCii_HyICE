e2ae9c0cf1e7aeffdc7f9ba9f24d30b540f02d47
zoebrasseur <44157316+zoebrasseur@users.noreply.github.com>
2018-11-22 10:28:14 +0200

    Add files via upload

---
+++ codes/step_1_test.m
@@ -0,0 +1,8 @@
+clear;
+clc;
+
+%% This code test the function load_raw_data
+
+inputDir='C:\Users\mittaaja\Desktop\PINCII\Matlab_codes\Common_code\raw_data';
+outputDir='C:\Users\mittaaja\Desktop\PINCII\Matlab_codes\Common_code\00_raw_data';
+res=load_raw_data(inputDir,outputDir);
