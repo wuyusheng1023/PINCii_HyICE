eb8097f5fd33bb301944a3d613ef3f16c6e0c2d7
zoebrasseur <44157316+zoebrasseur@users.noreply.github.com>
2018-11-23 16:06:55 +0200

    Delete step_1_test.m

--- codes/step_1_test.m
+++
@@ -1,8 +0,0 @@
-clear;
-clc;
-
-%% This code test the function load_raw_data
-
-inputDir='C:\Users\mittaaja\Desktop\PINCII\Matlab_codes\Common_code\raw_data';
-outputDir='C:\Users\mittaaja\Desktop\PINCII\Matlab_codes\Common_code\00_raw_data';
-res=load_raw_data(inputDir,outputDir);
